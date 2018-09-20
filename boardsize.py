cba9ae66e28d6164a2eb69e4c277a5f84025082a
Nicholas Li <nicknickli@berkeley.edu>
2018-09-19 19:39:45 -0700

    fixed board size selection

--- boardsize.py
+++ boardsize.py
@@ -14,12 +14,12 @@ def boardSizeMenu(menuWindow, oldFrame):
     #title
     titleText = StringVar()
     Label(boardFrame, textvariable = titleText).pack()
-    titleText.set("Board Size")
+    titleText.set("Select a board size")
 
     #buttons inside menu
-    board = StringVar()
-    boardSmall = Radiobutton(boardFrame, text = "15 x 15", value = 1, variable = board)
-    boardLarge = Radiobutton(boardFrame, text = "17 x 17", value = 2, variable = board)
+    board = IntVar()
+    boardSmall = Radiobutton(boardFrame, text = "15 x 15", variable = board, value = 1)
+    boardLarge = Radiobutton(boardFrame, text = "17 x 17", variable = board, value = 2)
     Button(boardFrame, text = "Confirm", command = lambda: boardSize(board.get(), menuWindow, boardFrame)).pack()
     Button(boardFrame, text = "Back", command = lambda: menu.mainMenu(menuWindow, boardFrame)).pack()
     #CAUTION: Can possibly overload menu window with lots of frames
@@ -31,11 +31,8 @@ def boardSizeMenu(menuWindow, oldFrame):
 
 def boardSize(value, menuWindow, boardFrame):
     if value == 1:
-        print("1")
-        return numpy.zeros(15, 15)
+        return numpy.zeros(15, 15) #this doesn't get saved, i need to start the game with another function
     elif value == 2:
         return numpy.zeros(17, 17)
-        print("2")
     else:
-        print("Please select a board size")
         boardSizeMenu(menuWindow, boardFrame)
--- twoplayer.py
+++ twoplayer.py
@@ -1,6 +1,5 @@
 #handles option twoplayers
 from boardsize import boardSizeMenu
 def twoPlayers(menu, oldFrame):
-    menuFrame.pack_forget()
     boardSizeMenu(menu, oldFrame)
     menu.destroy()
